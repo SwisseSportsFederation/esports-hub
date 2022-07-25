1ca4675fabdaf74628113f4df7b08b4b56ec3871
zischler <tobias.zischler@gmx.ch>
2022-07-25 16:08:00 -0300

    - add some content page text and add separate styling

--- app/components/ContentPageLayout.tsx
+++ app/components/ContentPageLayout.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 const ContentPageLayout = ({ children }: PropsWithChildren<{}>) => {
   return (
     <>
-      <div className="flex items-center justify-center h-5/6">
+      <div className="contentpage flex items-center justify-center h-5/6">
         <div className="prose dark:prose-dark p-7 max-w-prose">
           {children}
         </div>
--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,17 @@
 import type { LoaderFunction, MetaFunction } from "remix";
 import { json, Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "remix";
 import styles from "./styles/tailwind.css";
+import contentPageStyles from "~/styles/contentpage.css";
 import Footer from "~/components/Footer";
 import Header from "~/components/Header/Header";
 import { authenticator } from "~/services/auth.server";
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [{ rel: "stylesheet", href: styles }, { rel: "stylesheet", href: contentPageStyles }];
 }
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "SESF Esports Database" };
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
