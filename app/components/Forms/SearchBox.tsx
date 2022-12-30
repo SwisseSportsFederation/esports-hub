fc558ae24110eae26e10d05999ca43a3e48933ef
zischler <tobias.zischler@gmx.ch>
2022-12-30 16:03:59 +0100

    change search box styling to simpler variant

--- app/components/Forms/SearchBox.tsx
+++ app/components/Forms/SearchBox.tsx
@@ -1,70 +1,45 @@
-import Icon from "~/components/Icons";
 import { Form, useSearchParams } from "@remix-run/react";
 import ActionButton from "~/components/Button/ActionButton";
 import TextInput from "~/components/Forms/TextInput";
 import DropDownInput from "~/components/Forms/DropdownInput";
-import { CSSTransition } from 'react-transition-group';
-import { useState } from "react";
 
 type SearchBoxProps = ({
   games: string[];
   cantons: string[];
   languages: string[];
-  small?: never;
+  small?: boolean;
 } | {
   games?: never;
   cantons?: never;
   languages?: never;
-  small: boolean;
+  small?: never;
 });
 
 const SearchBox = ({ games = [], cantons = [], languages = [], small = false }: SearchBoxProps) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [params] = useSearchParams();
   const types: string[] = ["User", "Team", "Organisation"];
-  const filtersHTML = (
-    <>
-      <div className="grid grid-cols-2 gap-4 py-4">
-        <DropDownInput inputs={games} name="game" selected={params.get("game")}/>
-        <DropDownInput inputs={cantons} name="canton" selected={params.get("canton")}/>
-        <DropDownInput inputs={languages} name="language" selected={params.get("language")}/>
-        <DropDownInput inputs={types} name="type" selected={params.get("type")}/>
-      </div>
-      <div className="w-full pb-4">
-        <ActionButton content="Search" type="submit" className="mr-auto ml-auto"/>
-      </div>
-    </>
-  );
 
   return (
     <Form method="get" action={'/search'} autoComplete={"on"}>
-      <TextInput id="search" label="Search" searchIcon={true}
-                 buttonType="submit" defaultValue={params.get("search")}/>
-      {!small && <div className="flex items-center flex-col">
-        <CSSTransition
-          in={isDropdownOpen}
-          timeout={500}
-          classNames={{
-            enter: 'slide-y-enter',
-            enterActive: 'slide-y-enter-active',
-            enterDone: 'slide-y-enter-done',
-            exit: 'slide-y-exit',
-            exitActive: 'slide-y-exit-active'
-          }}>
-          <div className="bg-gray-6 dark:bg-gray-3 pt-5 px-3 -mt-5 rounded-b-[1.3rem] w-full overflow-hidden hidden">
-            {filtersHTML}
+      <div className="max-w-lg">
+        <TextInput id="search" label="Search" searchIcon={true}
+                  buttonType="submit" defaultValue={params.get("search")}/>
+      </div>
+      <div className="pt-5 px-3 -mt-5 w-full max-w-lg">
+        { !small && 
+          <div className="my-4 overflow-visible">
+            <div className="flex gap-4 overflow-visible justify-center">
+              <DropDownInput inputs={games} name="game" selected={params.get("game")}/>
+              <DropDownInput inputs={cantons} name="canton" selected={params.get("canton")}/>
+              <DropDownInput inputs={languages} name="language" selected={params.get("language")}/>
+              <DropDownInput inputs={types} name="type" selected={params.get("type")}/>
+            </div>
           </div>
-        </CSSTransition>
-        <button className="h-5 w-24 bg-gray-6 dark:bg-gray-3 rounded-b-xl focus:outline-none"
-                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                type="button">
-          <div className="max-w-full max-h-full flex items-center justify-center">
-            <Icon iconName='arrowDown'
-                  className={`h-6 w-6 transition-transform transform dark:text-white text-black 
-                  ${isDropdownOpen ? 'rotate-180' : ''}`}/>
-          </div>
-        </button>
-      </div>}
+        }
+        <div className="w-full pb-4">
+          <ActionButton content="Search" type="submit" className="mr-auto ml-auto"/>
+        </div>
+      </div>
     </Form>
   );
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,27 @@
 import SearchBox from "~/components/Forms/SearchBox";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import type { SearchParams } from "~/services/search.server";
+import { getSearchParams } from "~/services/search.server";
 
+type LoaderData = {
+  searchParams: SearchParams
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  return json({
+    searchParams: await getSearchParams()
+  });
+};
 export default function Index() {
+  const { searchParams } = useLoaderData<LoaderData>();
   return <>
     <section className="flex items-center justify-center flex-col mx-3">
       <h2 className="text-xl lg:text-3xl font-bold">Search for Swiss Esports Actors</h2>
-      <SearchBox small={false}/>
+      <SearchBox games={searchParams.games} cantons={searchParams.cantons ?? []}
+                languages={searchParams.languages ?? []}/>
     </section>
   </>;
 }
