03a32a680a6607028a3ddc4800598e6cb5927bee
zischler <tobias.zischler@gmx.ch>
2023-02-16 11:13:15 +0100

    add user validity check to admin panel

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,21 +1,33 @@
 import { json } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { Outlet, useLoaderData, useLocation, useNavigate } from "@remix-run/react";
 import Navbar from "~/components/Navbar";
-import { checkUserAuth } from "~/utils/auth.server";
+import { checkUserAuth, checkUserValid } from "~/utils/auth.server";
 import { getUserMemberships } from "~/services/admin/index.server";
 import type { LoaderFunctionArgs } from "@remix-run/router";
+import { useEffect } from "react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const user = await checkUserAuth(request);
+  const userValid = await checkUserValid(user.db.id, request.url);
   const memberships = await getUserMemberships(user);
   return json({
     user,
-    memberships
+    memberships,
+    userValid
   });
 }
 
 export default function Admin() {
-  const { user, memberships } = useLoaderData<typeof loader>();
+  const { user, memberships, userValid } = useLoaderData<typeof loader>();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!userValid) {
+      navigate('/admin/user/account');
+    }
+  }, [location]);
+
 
   return <>
     <Navbar/>
