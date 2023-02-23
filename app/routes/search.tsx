cd3b6d2299ed1afc8e8a22f338fb1a4e211ee73b
zischler <tobias.zischler@gmx.ch>
2023-02-23 10:55:37 +0100

    fixed search not cleaning after param change

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -25,40 +25,53 @@ export default function() {
   const fetcher = useFetcher();
   const maxLoadEntity = 5;
 
-  const [resultsNode, setResultsNode] = useState<JSX.Element[]>([]);
+  const [results, setResults] = useState<UserSearchResult[]>([]);
   const [users, setUsers] = useState<UserSearchResult[]>([]);
   const [teams, setTeams] = useState<UserSearchResult[]>([]);
   const [orgs, setOrgs] = useState<UserSearchResult[]>([]);
   const [hasNext, setHasNext] = useState<Boolean>(false);
 
-  const resultList = (userList: UserSearchResult[], teamList: UserSearchResult[], orgList: UserSearchResult[]) => {
-    setUsers([...users, ...userList]);
-    setTeams([...teams, ...teamList]);
-    setOrgs([...orgs, ...orgList]);
-  
+  const checkHasNext = (userList: UserSearchResult[], teamList: UserSearchResult[], orgList: UserSearchResult[]) => {
     setHasNext(userList.length >= maxLoadEntity || teamList.length >= maxLoadEntity || orgList.length >= maxLoadEntity);
+  }
+
+  const setEntities = (userList: UserSearchResult[], teamList: UserSearchResult[], orgList: UserSearchResult[]) => {
+    setUsers([...userList]);
+    setTeams([...teamList]);
+    setOrgs([...orgList]);
   
-    const results = new Array<UserSearchResult>().concat(teamList, orgList, userList);
-    return [...resultsNode, ...results.map((teaser: UserSearchResult, index: number) => 
-                <LinkTeaser key={index} id={teaser.id} name={teaser.name} team={teaser.team} games={teaser.games}
-                avatarPath={teaser.image} type={teaser.type} handle={teaser.handle}/>
-                )];
+    const newResults = new Array<UserSearchResult>().concat(teamList, orgList, userList);
+    return [...newResults];
   }
 
   useEffect(() => {
     if(fetcher.type === 'done') {
-      setResultsNode(resultList(fetcher.data.searchResults.users, fetcher.data.searchResults.teams, fetcher.data.searchResults.orgs));
+      const userList = fetcher.data.searchResults.users;
+      const teamList = fetcher.data.searchResults.teams;
+      const orgList = fetcher.data.searchResults.orgs;
+      checkHasNext(userList, teamList, orgList);
+      setEntities([...users, ...userList], [...teams, ...teamList], [...orgs, ...orgList]);
+      const newResults = new Array<UserSearchResult>().concat(teamList, orgList, userList);
+      setResults([...results, ...newResults]);
     }
   }, [fetcher.data])
 
   useEffect(() => {
-    setResultsNode(resultList(searchResults.users, searchResults.teams, searchResults.orgs));
-  }, [])
+    checkHasNext(searchResults.users, searchResults.teams, searchResults.orgs);
+    setEntities(searchResults.users, searchResults.teams, searchResults.orgs);
+    const newResults = new Array<UserSearchResult>().concat(searchResults.teams, searchResults.orgs, searchResults.users);
+    setResults([...newResults]);
+  }, [searchParams])
 
   return <div className="max-w-md w-full mx-auto px-4 pt-8">
-    <SearchBox games={searchParams.games} cantons={searchParams.cantons ?? []} languages={searchParams.languages ?? []}/>
-    {resultsNode && resultsNode}
-    {resultsNode && hasNext && <fetcher.Form method="get" action={'/search'} className="flex justify-center pt-4">
+    <SearchBox games={searchParams.games} cantons={searchParams.cantons ?? []}
+               languages={searchParams.languages ?? []}/>
+
+    {results && results.map((teaser: UserSearchResult, index: number) => 
+                <LinkTeaser key={index} id={teaser.id} name={teaser.name} team={teaser.team} games={teaser.games}
+                avatarPath={teaser.image} type={teaser.type} handle={teaser.handle}/>
+                )}
+    {results && hasNext && <fetcher.Form method="get" action={'/search'} className="flex justify-center pt-4">
       <input type="hidden" name="game" value={params.get("game")!}/>
       <input type="hidden" name="canton" value={params.get("canton")!}/>
       <input type="hidden" name="language" value={params.get("language")!}/>
