70a4db91fc02bcfe69ec87184c8dbc3ed1ae119f
zischler <tobias.zischler@gmx.ch>
2023-03-12 19:55:41 +0100

    update verify page styling

--- app/routes/auth/verify.tsx
+++ app/routes/auth/verify.tsx
@@ -4,8 +4,8 @@ import Icon from "~/components/Icons";
 const Verify = () => {
   return <>
     <div className='w-full my-auto flex flex-col items-center justify-center'>
+      <Icon iconName='envelope' className='text-color flex justify-center items-center w-16 max-w-1/2 mb-5'/>
       <H1>Check your mail to verify your account and log in again</H1>
-      <Icon iconName='envelope' className='text-color flex justify-center items-center w-1/2 mb-5'/>
     </div>
   </>;
 };
