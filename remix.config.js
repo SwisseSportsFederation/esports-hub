575c8a07d26fffec76a1e20ebfda26d6c380ce64
zischler <tobias.zischler@gmx.ch>
2024-01-21 17:44:11 +0100

    - remove unnecessary comments in remix config - change some dependencies for vercel

--- remix.config.js
+++ remix.config.js
@@ -1,4 +1,3 @@
-//import { createRoutesFromFolders } from "@remix-run/v1-route-convention";
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
@@ -10,8 +9,4 @@ export default {
   // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
 
-  /*routes(defineRoutes) {
-    // uses the v1 route convention, works in v1.15+ and v2
-    return createRoutesFromFolders(defineRoutes);
-  },*/
 };
