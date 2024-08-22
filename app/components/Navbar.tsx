a481fdd47f206487a5ab869088634d7d5a5706b6
zischler <tobias.zischler@gmx.ch>
2024-08-22 13:05:21 +0200

    - change navbar top margin - add feedback form link #150

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,26 +13,26 @@ interface ILinkBlockProps {
   icon?: IconType
 }
 
-const NavbarLink = ({path, title, icon = 'arrowDown'}: ILinkBlockProps) => {
+const NavbarLink = ({ path, title, icon = 'arrowDown' }: ILinkBlockProps) => {
   const iconClassNames = classNames({
     'transform -rotate-90': icon === 'arrowDown',
     'rounded-full bg-gray-100 dark:bg-gray-3': icon === 'edit'
   }, "p-0 m-0")
-  return <NavLink to={path} className={({isActive}) => 'text-color rounded-2xl hover:bg-gray-6 dark:hover:bg-gray-3' +
+  return <NavLink to={path} className={({ isActive }) => 'text-color rounded-2xl hover:bg-gray-6 dark:hover:bg-gray-3' +
     ' flex items-center justify-between px-4 py-1 mb-1 transition' + (isActive ? ' bg-gray-6 dark:bg-gray-3' : ' bg-white dark:bg-gray-2')}>
     {title}
     <div className={iconClassNames}>
-      <Icon iconName={icon} className='w-7 h-7'/>
+      <Icon iconName={icon} className='w-7 h-7' />
     </div>
   </NavLink>;
 }
 
 const membershipLinkBlock = (membership: Membership, type: EntityType) => {
-  if(membership.group_type === type) {
-    if(membership.access_rights === AccessRightValue.ADMINISTRATOR || membership.access_rights === AccessRightValue.MODERATOR) {
-      return <NavbarLink path={`/admin/${type.toLowerCase()}/${membership.handle}`} title={membership.name} icon="edit" key={membership.name}/>
+  if (membership.group_type === type) {
+    if (membership.access_rights === AccessRightValue.ADMINISTRATOR || membership.access_rights === AccessRightValue.MODERATOR) {
+      return <NavbarLink path={`/admin/${type.toLowerCase()}/${membership.handle}`} title={membership.name} icon="edit" key={membership.name} />
     }
-    return <NavbarLink path={`/detail/${type.toLowerCase()}/${membership.handle}`} title={membership.name} key={membership.name}/>
+    return <NavbarLink path={`/detail/${type.toLowerCase()}/${membership.handle}`} title={membership.name} key={membership.name} />
   }
   return null
 }
@@ -40,37 +40,37 @@ const membershipLinkBlock = (membership: Membership, type: EntityType) => {
 export default function Navbar() {
   const { memberships, isSuperAdmin } = useLoaderData();
 
-  return <div className="hidden lg:block h-full z-10 fixed top-16 bg-white dark:bg-gray-2 w-72 text-color p-4 overflow-y-auto">
+  return <div className="hidden lg:block h-full z-10 fixed top-20 bg-white dark:bg-gray-2 w-72 text-color p-4 overflow-y-auto">
     <div className="mb-16">
       <div className="text-xl font-bold mb-4">User</div>
-      <NavbarLink path={`/admin/user/account`} title="Account"/>
-      <NavbarLink path={`/admin/user/games`} title="Games"/>
-      <NavbarLink path={`/admin/user/socials`} title="Socials"/>
-      <NavbarLink path={`/admin/teams`} title="My Teams"/>
-      <NavbarLink path={`/admin/organisations`} title="My Organisations"/>
+      <NavbarLink path={`/admin/user/account`} title="Account" />
+      <NavbarLink path={`/admin/user/games`} title="Games" />
+      <NavbarLink path={`/admin/user/socials`} title="Socials" />
+      <NavbarLink path={`/admin/teams`} title="My Teams" />
+      <NavbarLink path={`/admin/organisations`} title="My Organisations" />
     </div>
     <div className="mb-16">
       <div className="text-xl font-bold mb-4 flex">
         <span>Teams</span>
         <div className="ml-3">
-          <IconButton icon={"add"} type='link' path="/admin/create/team" size="small"/>
+          <IconButton icon={"add"} type='link' path="/admin/create/team" size="small" />
         </div>
       </div>
-      {  memberships.groups.map((group: Membership) => membershipLinkBlock(group, EntityTypeValue.TEAM)) }
+      {memberships.groups.map((group: Membership) => membershipLinkBlock(group, EntityTypeValue.TEAM))}
     </div>
     <div className="mb-16">
       <div className="text-xl font-bold mb-4 flex">
         <span>Organisations</span>
         <div className="ml-3">
-          <IconButton icon={"add"} type='link' path="/admin/create/organisation" size="small"/>
+          <IconButton icon={"add"} type='link' path="/admin/create/organisation" size="small" />
         </div>
       </div>
-      { memberships.groups.map((group: Membership) => membershipLinkBlock(group, EntityTypeValue.ORGANISATION))}
+      {memberships.groups.map((group: Membership) => membershipLinkBlock(group, EntityTypeValue.ORGANISATION))}
     </div>
-    { isSuperAdmin && 
+    {isSuperAdmin &&
       <div className="mb-16">
         <div className="text-xl font-bold mb-4">Superadmin</div>
-        <NavbarLink path={`/superadmin`} title="Superadmin"/>
+        <NavbarLink path={`/superadmin`} title="Superadmin" />
       </div>
     }
   </div>
