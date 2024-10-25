16e907bea16329b82c17b3c5bd170223ad5d19f1
zischler <tobias.zischler@gmx.ch>
2024-10-25 10:19:55 +0200

    fix scroll in social select list. fix #187

--- app/components/SelectList.tsx
+++ app/components/SelectList.tsx
@@ -24,7 +24,7 @@ const SelectListItem = ({ icon, title, onClick }: ISelectListItemProps) => {
   return <li
     className='flex p-5 border-b border-gray-300 dark:border-black cursor-pointer hover:text-red-1 transition-colors capitalize'
     onClick={onClick}>
-    <Icons iconName={icon} className='w-8 h-8 mr-5'/>
+    <Icons iconName={icon} className='w-8 h-8 mr-5' />
     {title.toLowerCase()}
   </li>;
 };
@@ -35,7 +35,7 @@ const SelectList = ({ values, onSelect, showButton }: ISelectListProps) => {
 
   return <>
     {values.length > 0 && showButton &&
-      <IconButton icon='add' action={() => setVisible(true)} size='medium' type='button'/>}
+      <IconButton icon='add' action={() => setVisible(true)} size='medium' type='button' />}
     <CSSTransition
       in={visible}
       timeout={200}
@@ -49,17 +49,22 @@ const SelectList = ({ values, onSelect, showButton }: ISelectListProps) => {
       nodeRef={nodeRef}
     >
       <div className="fixed w-full h-full left-0 bottom-0 bg-black z-[51] bg-opacity-25 overflow-hidden hidden"
-           onClick={() => setVisible(false)} ref={nodeRef}>
-        <ul className="absolute bottom-0 bg-white dark:bg-gray-2 w-full rounded-t-2xl text-2xl px-5 pb-20 transform
-            lg:w-1/2 lg:!translate-y-1/2 lg:bottom-1/2 lg:rounded-2xl lg:pb-0 lg:!-translate-x-1/2 lg:left-1/2 ">
-          {
-            values.map((value: SocialPlatform) => {
-              // add timeout so the UI doesn't jump around..... fml
-              return <SelectListItem key={value} icon={value.toLowerCase() as IconType} title={value}
-                                     onClick={() => setTimeout(() => onSelect(value), 200)}/>;
-            })
-          }
-        </ul>
+        onClick={() => setVisible(false)} ref={nodeRef}>
+        <div className="absolute bottom-0 bg-white dark:bg-gray-2 w-full rounded-t-2xl text-2xl px-5 pb-20 transform
+            lg:w-1/2 lg:!translate-y-1/2 lg:bottom-1/2 lg:rounded-2xl lg:pb-0 lg:!-translate-x-1/2 lg:left-1/2 max-h-screen overflow-y-scroll">
+          <div className="relative pt-4">
+            <IconButton icon='remove' action={() => setVisible(false)} size='medium' type='button' className="absolute top-4 right-4" />
+            <ul>
+              {
+                values.map((value: SocialPlatform) => {
+                  // add timeout so the UI doesn't jump around..... fml
+                  return <SelectListItem key={value} icon={value.toLowerCase() as IconType} title={value}
+                    onClick={() => setTimeout(() => onSelect(value), 200)} />;
+                })
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     </CSSTransition>
   </>;
