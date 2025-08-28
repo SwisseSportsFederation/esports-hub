769b00a4128a2fb1c169404a1b40688d54db6f37
Tobias Zischler <tobias.zischler@gmx.ch>
2025-08-28 12:25:27 +0200

    add become a member banner

--- app/routes/house+/_index.tsx
+++ app/routes/house+/_index.tsx
@@ -2,6 +2,7 @@ import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json, useLoaderData } from '@remix-run/react';
 import { isLoggedIn } from "~/utils/auth.server";
 import { db } from "~/services/db.server";
+import LinkButton from "~/components/Button/LinkButton";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
 	const loggedIn = await isLoggedIn(request);
@@ -45,7 +46,12 @@ export default function () {
 	return <div className="mx-3 py-7">
 		<div className="max-w-prose lg:max-w-6xl w-full mx-auto">
 			<h1 className="text-4xl font-bold mb-2">Esports House</h1>
-			<p className="text-lg">The Esports House project is a project by the Swiss Esports Federation to help esports teams in Switzerland find affordable bootcamp locations. We have partnered with various locations in Switzerland to make this project possible. <b className="font-bold">SESF members gain an additional discount to the packages. <a className="underline text-red-500" href="https://sesf.ch/become-a-member/">Become a SESF member</a></b></p>
+			<p className="text-lg">The Esports House project is a project by the Swiss Esports Federation to help esports teams in Switzerland find affordable bootcamp locations. We have partnered with various locations in Switzerland to make this project possible. </p>
+			<div className="bg-gray-6 dark:bg-gray-2 p-6 rounded-lg my-8">
+				<h3 className="text-2xl dark:text-white font-bold mb-1">Become an SESF member</h3>
+				<p className="dark:text-white mb-4">Join the Swiss Esports Federation to get access to exclusive benefits and up to CHF 400.- in discounts per year.</p>
+				<LinkButton path="https://sesf.ch/become-a-member/" title="Become a member" />
+			</div>
 			<h2 className="mt-8 mb-4 font-bold text-4xl">Locations</h2>
 			<div className="grid grid-cols-3 gap-y-4 lg:gap-12">
 				{locations.map((location) => {
