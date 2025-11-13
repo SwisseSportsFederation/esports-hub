d0184918e6b059d4e25a76de899b9b18bc9307f2
Tobias Zischler <tobias.zischler@gmx.ch>
2025-11-13 21:29:31 +0100

    update some seed commands for prisma #180

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,4 @@
-import type { Prisma } from "@prisma/client";
-import { AccessRight, PrismaClient, RequestStatus, SocialPlatform, VerificationLevel } from "@prisma/client";
-import { faker } from '@faker-js/faker';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -36,7 +34,7 @@ async function seed() {
       { name: "Jura" }
     ]
   });
-
+  
   await prisma.game.createMany({
     data: [
       { name: 'League of Legends', is_active: true },
@@ -59,146 +57,6 @@ async function seed() {
       { name: 'Português' },
     ]
   });
-
-  await Promise.all(createOrgs().map(data => prisma.group.create({ data })));
-  await Promise.all(createTeams().map(data => prisma.group.create({ data })));
-  await Promise.all(createUsers().map(data => prisma.user.create({ data })));
-  const user = createUsers()[0];
-  await prisma.user.create({
-    data: {
-      ...user,
-      auth_id: 'auth0|63ac8a2f73084ca5370c4aed',
-      email: 'test@test.com'
-    }
-  });
 }
 
 seed();
-
-function fakeBigInt(min: number, max: number) {
-  return faker.number.bigInt({
-    min,
-    max
-  })
-}
-
-function common() {
-  const languagesCount = faker.number.int({
-    min: 1,
-    max: 3
-  });
-
-  return {
-    handle: faker.person.firstName(),
-    verification_level: faker.helpers.objectValue(VerificationLevel),
-    name: faker.company.name(),
-    description: faker.lorem.lines(3),
-    socials: {
-      create: [{
-        name: faker.company.name(),
-        platform: faker.helpers.objectValue(SocialPlatform)
-      }]
-    },
-    languages: {
-      connect: Array.from({ length: languagesCount }).map(() => {
-        return {
-          id: fakeBigInt(1, 6)
-        }
-      })
-    },
-    canton: {
-      connect: {
-        id: fakeBigInt(1, 26)
-      }
-    }
-  }
-}
-
-const array = (max: number = 4) => Array.from({ length: max });
-
-
-function createUsers(): Prisma.UserCreateInput[] {
-  return array(10).map(() => {
-    return {
-      ...common(),
-      email: faker.internet.email(),
-      surname: faker.person.lastName(),
-      groups: {
-        createMany: {
-          data: createGroupMember()
-        }
-      },
-      former_teams: {
-        createMany: {
-          data: createFormerTeams()
-        }
-      },
-      games: {
-        connect: array(3).map(() => {
-          return {
-            id: fakeBigInt(1, 7)
-          }
-        })
-      }
-    }
-  });
-}
-
-
-function createFormerTeams(): Prisma.FormerTeamCreateManyUserInput[] {
-  return array().map(() => {
-    return {
-      name: faker.company.name(),
-      from: faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2021-01-01T00:00:00.000Z' }),
-      to: faker.date.between({ from: '2022-01-01T00:00:00.000Z', to: '2024-01-01T00:00:00.000Z' }),
-    }
-  });
-
-}
-
-function createGroupMember(): Prisma.GroupMemberCreateManyUserInput[] {
-  return array().map((_, index) => {
-    return {
-      access_rights: faker.helpers.objectValue(AccessRight),
-      is_main_group: index === 0,
-      request_status: faker.helpers.arrayElement([RequestStatus.ACCEPTED, RequestStatus.PENDING_USER, RequestStatus.PENDING_GROUP]),
-      joined_at: faker.date.between({ from: '2018-01-01T00:00:00.000Z', to: '2021-01-01T00:00:00.000Z' }),
-      role: faker.person.jobTitle(),
-      group_id: index + 1
-    }
-  });
-}
-
-function createTeams(): Prisma.GroupCreateInput[] {
-  return array(10).map(() => {
-    return {
-      ...common(),
-      game: {
-        connect: {
-          id: fakeBigInt(1, 7)
-        }
-      },
-      group_type: "TEAM",
-      parent: {
-        create: {
-          request_status: faker.helpers.arrayElement([RequestStatus.ACCEPTED, RequestStatus.PENDING_GROUP, RequestStatus.PENDING_PARENT_GROUP]),
-          parent: {
-            connect: {
-              id: fakeBigInt(1, 10)
-            }
-          }
-        }
-      }
-    }
-  });
-}
-
-function createOrgs(): Prisma.GroupCreateInput[] {
-  return array(10).map(() => {
-    return {
-      ...common(),
-      group_type: "ORGANISATION",
-    }
-  })
-}
-
